078ed51235bdbcdfd95e0233c05d256888ac507f
singhayushh <coding.ayush@gmail.com>
2021-07-01 18:47:12 +0530

    custom middlewares and utility functions

---
+++ support/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const auth = (passer) => {
+    return async (req, res, next) => {
+        try {
+            const decoded = jwt.verify(req.cookies['auth_token'], process.env.JWT_SECRET);
+            req.body.user = decoded;
+            if (passer == 'block')
+                res.redirect('/');            
+            else if (passer == 'admin') {
+                if (decoded.role != 'Admin')
+                    res.status(401).send({ "message": "You are not authorized to use this service" }); 
+                else 
+                    next();
+            } else
+                next();                 
+        } catch (error) {
+            res.redirect('/login?status=unauthorized');
+        }
+    };
+}
+
+module.exports = auth;
