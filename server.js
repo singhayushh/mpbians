e82e99f0b787caa604d0c62ab408bd1cf93cf7ee
singhayushh <coding.ayush@gmail.com>
2021-07-02 22:36:43 +0530

    code formatting

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // Include packages
-const express = require('express');
-const cors = require('cors');
-const { connect, connection } = require('mongoose');
-const cookieParser = require('cookie-parser');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { connect, connection } = require("mongoose");
+const cookieParser = require("cookie-parser");
+require("dotenv").config();
 
 // Initialize express
 const app = express();
@@ -12,30 +12,37 @@ const baseurl = process.env.base_url;
 
 // Databse configuration
 const uri = String(process.env.mongo_uri);
-const connectOptions = { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false };
-connect(uri, connectOptions).then().catch((err) => console.log('Error:' + err));
-connection.once('open', () => console.log('DB Connection established'));
+const connectOptions = {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+};
+connect(uri, connectOptions)
+    .then()
+    .catch((err) => console.log("Error:" + err));
+connection.once("open", () => console.log("DB Connection established"));
 
 // Setup middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(__dirname + '/views'));
-app.set('case sensitive routing', true);
-app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/views"));
+app.set("case sensitive routing", true);
+app.set("view engine", "ejs");
 
 // Include routes
-const mainRoutes = require('./main/route');
-const userRoutes = require('./user/route');
-const profileRoutes = require('./profile/route');
+const mainRoutes = require("./main/route");
+const userRoutes = require("./user/route");
+const profileRoutes = require("./profile/route");
 
-app.use('/', mainRoutes);
-app.use('/user', userRoutes);
-app.use('/profile', profileRoutes);
+app.use("/", mainRoutes);
+app.use("/user", userRoutes);
+app.use("/profile", profileRoutes);
 
-app.get('*', (_req, res) => {
-    res.render('404');
+app.get("*", (_req, res) => {
+    res.render("404");
 });
 
 app.listen(port, () => console.log(`Website running at ${baseurl}:${port}`));
